6e91dae654aa8b3b873b1d2db48150f6f2889998
Hannah Fowler <t-hafow@microsoft.com>
2018-07-24 13:56:49 -0700

    adding some frontend

--- src/index.js
+++ src/index.js
@@ -2,3 +2,13 @@ export class Index {
   static init() {
   }
 }
+
+<header class="masthead">
+        <div class="inner">
+          <h3 class="masthead-brand">ISN</h3>
+          <nav class="nav nav-masthead justify-content-center">
+            <a class="nav-link active" href="#">Home</a>
+            <a class="nav-link" href="#">t-hafow@microsoft.com<img class="profile-img" src="./lib/images/headshot.png"></a>
+          </nav>
+        </div>
+</header>

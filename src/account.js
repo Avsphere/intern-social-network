de526e1ced2399f1d7c51d285e76f5520d499c09
Ivraj Seerha <iseerha@college.harvard.edu>
2018-07-25 10:56:35 -0700

    Removes footer on add project for account page

--- src/account.js
+++ src/account.js
@@ -1,19 +1,20 @@
 import axios from 'axios'
 export class Account {
   constructor() {
-    this.initHandlers();
+    this.initHandlers()
   }
   helloworld() {
-    console.log("Hello world from account");
+    console.log('Hello world from account')
   }
 
   returnDummyFormData() {
     return {
-      team: "aarons team",
+      team: 'aarons team',
       org: 'awesome org',
-      projects: [{
-          title: "Awesome project",
-          description: "A cool ass project yo",
+      projects: [
+        {
+          title: 'Awesome project',
+          description: 'A cool ass project yo',
           conceptTags: ['IoT', 'cloud'],
           techStackTags: ['ps', 'js', 'css'],
           timeDistribution: {
@@ -21,12 +22,12 @@ export class Account {
             devTime: 3,
             designTime: 1,
             emailTime: 1,
-            writingTime: 3
-          }
+            writingTime: 3,
+          },
         },
         {
-          title: "Woah woah project",
-          description: "A cool ass project yo",
+          title: 'Woah woah project',
+          description: 'A cool ass project yo',
           conceptTags: ['Mixed Reality', 'Design'],
           techStackTags: ['ps', 'js', 'css'],
           timeDistribution: {
@@ -34,39 +35,41 @@ export class Account {
             devTime: 3,
             designTime: 11,
             emailTime: 11,
-            writingTime: 3
-          }
-        }
-      ]
-
+            writingTime: 3,
+          },
+        },
+      ],
     }
   }
 
   grabFormData() {
-    return this.returnDummyFormData();
+    return this.returnDummyFormData()
   }
 
   updateAccount() {
     return new Promise((resolve, reject) => {
       let data = {
         formData: this.grabFormData(),
-        userId: $('#titleHeader').attr('data-account-id')
+        userId: $('#titleHeader').attr('data-account-id'),
       }
-      axios.post('/updateUser', data).then((res) => {
-        if (res.statusText === 'OK') {
-          resolve(res.data);
-        } else {
-          console.log("FAILED", res);
-        }
-      }).catch((e) => {
-        console.log("ERROR in request runner login", err);
-        resolve(err);
-      })
+      axios
+        .post('/updateUser', data)
+        .then(res => {
+          if (res.statusText === 'OK') {
+            resolve(res.data)
+          } else {
+            console.log('FAILED', res)
+          }
+        })
+        .catch(e => {
+          console.log('ERROR in request runner login', err)
+          resolve(err)
+        })
     })
   }
 
   addProjectHtml() {
-    console.log("Running html fun")
+    console.log('Running html fun')
 
     $('#projectSection').append(`
       <div class="Account__newProject">
@@ -105,22 +108,21 @@ export class Account {
           </form> 
         </div>
       </div>
-    `);
+    `)
+    $('#footer').remove()
   }
 
   initHandlers() {
-    let that = this;
+    let that = this
 
-    $('#submitForm').on('click', (el) => {
-      el.preventDefault();
-      that.updateAccount();
+    $('#submitForm').on('click', el => {
+      el.preventDefault()
+      that.updateAccount()
     })
 
-    $('#addProject').on('click', (e) => {
-      e.preventDefault();
-      that.addProjectHtml();
+    $('#addProject').on('click', e => {
+      e.preventDefault()
+      that.addProjectHtml()
     })
   }
-
-
 }

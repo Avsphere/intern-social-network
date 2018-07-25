b4a38e7a5ec1aae8f3589ec5d898a79954012d61
Ivraj Seerha <iseerha@college.harvard.edu>
2018-07-25 08:57:42 -0700

    Adds prettier and about placeholder

--- routing/masterRouter.js
+++ routing/masterRouter.js
@@ -1,33 +1,33 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 const passport = require('passport')
 const path = require('path')
-const utils = require('../utils/utils.js');
-const User = require('../models/user');
+const utils = require('../utils/utils.js')
+const User = require('../models/user')
 
-const frontendDev = true;
+const frontendDev = true
 
 if (frontendDev) {
   router.get('/', (req, res) => {
     res.render('index', {
-      user: User.dummyData
+      user: User.dummyData,
     })
   })
 }
 
 router.get('/', (req, res) => {
   if (!req.isAuthenticated()) {
-    res.redirect('/login');
+    res.redirect('/login')
   } else {
-    let profileData = req.user.profile;
+    let profileData = req.user.profile
     res.render('index', {
-      user: profileData
-    });
+      user: profileData,
+    })
   }
-});
+})
 
-router.get('/account', utils.checkAuthenticated, function (req, res) {
-  let profileData = req.user.profile;
+router.get('/account', utils.checkAuthenticated, function(req, res) {
+  let profileData = req.user.profile
   if (!profileData.team) {
     profileData.team = 'Enter Team Name'
   }
@@ -35,86 +35,94 @@ router.get('/account', utils.checkAuthenticated, function (req, res) {
     profileData.org = 'Enter Org Name'
   }
   res.render('account', {
-    user: profileData
-  });
+    user: profileData,
+  })
 })
 
-router.get('/blogTest', function (req, res) {
-  res.render('blogTest');
+router.get('/about', function(req, res) {
+  res.render('about')
 })
 
+router.get('/blogTest', function(req, res) {
+  res.render('blogTest')
+})
 
-router.get('/login',
+router.get(
+  '/login',
   passport.authenticate('azuread-openidconnect', {
-    failureRedirect: '/'
+    failureRedirect: '/',
   }),
   (req, res) => {
-    res.redirect('/');
-  });
-
-router.get('/token',
-  passport.authenticate('azuread-openidconnect', {
-    failureRedirect: '/'
-  }),
-  (req, res) => {
-    let blanketScoped_userData;
-    utils.getUserData(req.user.accessToken).then((userData) => {
-      blanketScoped_userData = userData;
-      return utils.findUserByUPN(userData.body.onPremisesUserPrincipalName);
-    }).then((foundUser) => {
-      if (foundUser) {
-        return new Promise((resolve, reject) => {
-          resolve(foundUser);
-        })
-      } else {
-        return utils.createNewUser({
-          firstName: blanketScoped_userData.body.givenName,
-          surname: blanketScoped_userData.body.surname,
-          displayName: blanketScoped_userData.body.displayName,
-          department: blanketScoped_userData.body.department,
-          jobTitle: blanketScoped_userData.body.jobTitle,
-          upn: blanketScoped_userData.body.onPremisesUserPrincipalName
-        })
-      }
-    }).then((mongoUser) => {
-      req.user.profile = mongoUser;
-      res.render('index', {
-        user: req.user.profile
-      })
-    }).catch((e) => {
-      e.innerError = (e.response) ? e.response.text : '';
-      res.render('error', {
-        error: e
-      });
-    })
+    res.redirect('/')
   }
-);
+)
+
+router.get(
+  '/token',
+  passport.authenticate('azuread-openidconnect', {
+    failureRedirect: '/',
+  }),
+  (req, res) => {
+    let blanketScoped_userData
+    utils
+      .getUserData(req.user.accessToken)
+      .then(userData => {
+        blanketScoped_userData = userData
+        return utils.findUserByUPN(userData.body.onPremisesUserPrincipalName)
+      })
+      .then(foundUser => {
+        if (foundUser) {
+          return new Promise((resolve, reject) => {
+            resolve(foundUser)
+          })
+        } else {
+          return utils.createNewUser({
+            firstName: blanketScoped_userData.body.givenName,
+            surname: blanketScoped_userData.body.surname,
+            displayName: blanketScoped_userData.body.displayName,
+            department: blanketScoped_userData.body.department,
+            jobTitle: blanketScoped_userData.body.jobTitle,
+            upn: blanketScoped_userData.body.onPremisesUserPrincipalName,
+          })
+        }
+      })
+      .then(mongoUser => {
+        req.user.profile = mongoUser
+        res.render('index', {
+          user: req.user.profile,
+        })
+      })
+      .catch(e => {
+        e.innerError = e.response ? e.response.text : ''
+        res.render('error', {
+          error: e,
+        })
+      })
+  }
+)
 
 router.get('/disconnect', (req, res) => {
   req.session.destroy(() => {
-    req.logOut();
-    res.clearCookie('graphNodeCookie');
-    res.status(200);
-    res.redirect('/');
-  });
-});
-
+    req.logOut()
+    res.clearCookie('graphNodeCookie')
+    res.status(200)
+    res.redirect('/')
+  })
+})
 
 router.post('/updateUser', (req, res) => {
   let userId = req.body.userId,
-    userData = req.body.formData;
-  User.findById(userId, function (err, user) {
+    userData = req.body.formData
+  User.findById(userId, function(err, user) {
     if (!err) {
-      user.team = userData.team;
-      user.org = userData.org;
-      let projectIds = utils.createProjects(userId, userData.projects);
-      res.send(user);
+      user.team = userData.team
+      user.org = userData.org
+      let projectIds = utils.createProjects(userId, userData.projects)
+      res.send(user)
     } else {
-      console.log("Problem in update user!");
+      console.log('Problem in update user!')
     }
   })
 })
 
-
-
-module.exports = router;
+module.exports = router
